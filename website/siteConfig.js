08d8f31a0d7b31ad8299bcb2f6c5c3377e384689
Christa <64300342+cochranc@users.noreply.github.com>
2020-10-03 13:06:17 -0500

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -72,11 +72,9 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
-        twitter: 'https://twitter.com/',
-        github: 'https://github.com/',
-        //facebook: 'https://www.facebook.com/',
-        //instagram: 'https://www.instagram.com/',
+        linkedin: 'https://www.linkedin.com/in/christacochran2022/',
+        github: 'https://github.com/cochranc',
+        instagram: 'https://www.instagram.com/christacochranphoto/',
         //youtube: 'https://www.youtube.com/',
         //twitch: 'https://www.twitch.tv/'
     },
